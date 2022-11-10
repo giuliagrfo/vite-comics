1bff75e3fee052ea2dd2d5cee2e83be831feb1a1
giuliagrfo <giuliagrifo1@gmail.com>
2022-11-10 17:56:15 +0100

    add call to action

--- src/components/SiteFooter.vue
+++ src/components/SiteFooter.vue
@@ -8,122 +8,143 @@ export default {
 
 <template>
     <footer id="site_footer">
-        <div class="container">
-            <div class="footer_top">
-                <div class="row">
-                    <div class="col links">
-                        <div class="row">
-                            <div class="col dc_comics g-5">
-                                <h3>Dc Comics</h3>
-                                <ul>
-                                    <li>
-                                        <a href="#">Characters</a>
-                                    </li>
-                                    <li>
-                                        <a href="#">Comics</a>
-                                    </li>
-                                    <li>
-                                        <a href="#">Movies</a>
-                                    </li>
-                                    <li>
-                                        <a href="#">Tv</a>
-                                    </li>
-                                    <li>
-                                        <a href="#">Games</a>
-                                    </li>
-                                    <li>
-                                        <a href="#">Videos</a>
-                                    </li>
-                                    <li>
-                                        <a href="#">News</a>
-                                    </li>
-                                </ul>
-                                <div class="row">
-                                    <div class="col shop">
-                                        <h3>Shop</h3>
-                                        <ul>
-                                            <li>
-                                                <a href="#">Shop DC</a>
-                                            </li>
-                                            <li>
-                                                <a href="#">Shop DC Collectibles</a>
-                                            </li>
 
-                                        </ul>
-                                    </div>
+        <div class="container">
+            <div class="row">
+                <div class="col links">
+                    <div class="row">
+                        <div class="col dc_comics gy-5">
+                            <h3>Dc Comics</h3>
+                            <ul>
+                                <li>
+                                    <a href="#">Characters</a>
+                                </li>
+                                <li>
+                                    <a href="#">Comics</a>
+                                </li>
+                                <li>
+                                    <a href="#">Movies</a>
+                                </li>
+                                <li>
+                                    <a href="#">Tv</a>
+                                </li>
+                                <li>
+                                    <a href="#">Games</a>
+                                </li>
+                                <li>
+                                    <a href="#">Videos</a>
+                                </li>
+                                <li>
+                                    <a href="#">News</a>
+                                </li>
+                            </ul>
+                            <div class="row">
+                                <div class="col shop">
+                                    <h3>Shop</h3>
+                                    <ul>
+                                        <li>
+                                            <a href="#">Shop DC</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">Shop DC Collectibles</a>
+                                        </li>
+
+                                    </ul>
                                 </div>
                             </div>
-                            <div class="col dc g-5">
-                                <h3>Dc</h3>
-                                <ul>
-                                    <li>
-                                        <a href="#">Terms Of Use</a>
-                                    </li>
-                                    <li>
-                                        <a href="#">Privacy Policy</a>
-                                    </li>
-                                    <li>
-                                        <a href="#">Ad Choices</a>
-                                    </li>
-                                    <li>
-                                        <a href="#">Advertising</a>
-                                    </li>
-                                    <li>
-                                        <a href="#">Jobs</a>
-                                    </li>
-                                    <li>
-                                        <a href="#">Subscriptions</a>
-                                    </li>
-                                    <li>
-                                        <a href="#">Talent Workshops</a>
-                                    </li>
-                                    <li>
-                                        <a href="#">CPSC Certificates</a>
-                                    </li>
-                                    <li>
-                                        <a href="#">Ratings</a>
-                                    </li>
-                                    <li>
-                                        <a href="#">Shop Help</a>
-                                    </li>
-                                    <li>
-                                        <a href="#">Contact Us</a>
-                                    </li>
-                                </ul>
-                            </div>
-                            <div class="col sites g-5">
-                                <h3>Sites</h3>
-                                <ul>
-                                    <li>
-                                        <a href="#">DC</a>
-                                    </li>
-                                    <li>
-                                        <a href="#">MAD Magazine</a>
-                                    </li>
-                                    <li>
-                                        <a href="#">DC Kids</a>
-                                    </li>
-                                    <li>
-                                        <a href="#">DC Universe</a>
-                                    </li>
-                                    <li>
-                                        <a href="#">DC Power Visa</a>
-                                    </li>
+                        </div>
+                        <div class="col dc gy-5">
+                            <h3>Dc</h3>
+                            <ul>
+                                <li>
+                                    <a href="#">Terms Of Use</a>
+                                </li>
+                                <li>
+                                    <a href="#">Privacy Policy</a>
+                                </li>
+                                <li>
+                                    <a href="#">Ad Choices</a>
+                                </li>
+                                <li>
+                                    <a href="#">Advertising</a>
+                                </li>
+                                <li>
+                                    <a href="#">Jobs</a>
+                                </li>
+                                <li>
+                                    <a href="#">Subscriptions</a>
+                                </li>
+                                <li>
+                                    <a href="#">Talent Workshops</a>
+                                </li>
+                                <li>
+                                    <a href="#">CPSC Certificates</a>
+                                </li>
+                                <li>
+                                    <a href="#">Ratings</a>
+                                </li>
+                                <li>
+                                    <a href="#">Shop Help</a>
+                                </li>
+                                <li>
+                                    <a href="#">Contact Us</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col sites gy-5">
+                            <h3>Sites</h3>
+                            <ul>
+                                <li>
+                                    <a href="#">DC</a>
+                                </li>
+                                <li>
+                                    <a href="#">MAD Magazine</a>
+                                </li>
+                                <li>
+                                    <a href="#">DC Kids</a>
+                                </li>
+                                <li>
+                                    <a href="#">DC Universe</a>
+                                </li>
+                                <li>
+                                    <a href="#">DC Power Visa</a>
+                                </li>
 
-                                </ul>
-                            </div>
+                            </ul>
                         </div>
                     </div>
+                </div>
 
-                    <div class="col ft_logo">
-                        <img src="../assets/img/dc-logo-bg.png" alt="">
-                    </div>
+                <div class="col ft_logo">
+                    <img src="../assets/img/dc-logo-bg.png" alt="">
                 </div>
             </div>
+        </div>
 
-            <div class="footer_bottom">
+
+        <div class="call_to_action">
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <div class="sign_up">
+                            <a href="#">Sign-Up Now!</a>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="social">
+                            <h3>Follow Us</h3>
+                            <img src="../assets/img/footer-facebook.png" alt="">
+                            <img src="../assets/img/footer-twitter.png" alt="">
+                            <img src="../assets/img/footer-youtube.png" alt="">
+                            <img src="../assets/img/footer-pinterest.png" alt="">
+                            <img src="../assets/img/footer-periscope.png" alt="">
+                        </div>
+
+                    </div>
+                </div>
 
             </div>
+
         </div>
 
 
