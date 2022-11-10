b9173c8d7a106f3a8c99c25bac7cedbaea546b7f
giuliagrfo <giuliagrifo1@gmail.com>
2022-11-10 16:16:47 +0100

    add header

--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -7,7 +7,26 @@ export default {
 </script>
 
 <template>
-    <header id="site_header">
+    <div class="container">
+        <header id="site_header">
+            <div class="logo">
+                <img src="../assets/img/dc-logo.png" alt="">
+            </div>
+            <nav class="navbar">
+                <a href="#">Characters</a>
+                <a href="#" class="active">Comics</a>
+                <a href="#">Movies</a>
+                <a href="#">Tv</a>
+                <a href="#">Games</a>
+                <a href="#">Collectibles</a>
+                <a href="#">Videos</a>
+                <a href="#">Fans</a>
+                <a href="#">News</a>
+                <a href="#">Shop</a>
+            </nav>
 
-    </header>
+
+        </header>
+    </div>
+
 </template>
