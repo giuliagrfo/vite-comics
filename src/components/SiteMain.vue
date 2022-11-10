f87febfb12000cdc799f1579b316101c9119bd67
giuliagrfo <giuliagrifo1@gmail.com>
2022-11-10 16:47:05 +0100

    add main - jumbotron

--- src/components/SiteMain.vue
+++ src/components/SiteMain.vue
@@ -8,6 +8,38 @@ export default {
 
 <template>
     <main id="site_main">
+        <div class="jumbotron">
+            <div class="content">
+                <div class="container">
+                    <h2>Content</h2>
+                </div>
+            </div>
+            <nav class="navbar">
+                <div class="container">
+                    <a href="#">
+                        <img src="../assets/img/buy-comics-digital-comics.png" alt="">
+                        Digital Comic
+                    </a>
+                    <a href="#">
+                        <img src="../assets/img/buy-comics-merchandise.png" alt="">
+                        DC Merchandise
+                    </a>
+                    <a href="#">
+                        <img src="../assets/img/buy-comics-subscriptions.png" alt="">
+                        Subscription
+                    </a>
+                    <a href="#">
+                        <img src="../assets/img/buy-comics-shop-locator.png" alt="">
+                        Comic Shop Locator
+                    </a>
+                    <a href="#">
+                        <img src="../assets/img/buy-dc-power-visa.svg" alt="">
+                        DC Power Visa
+                    </a>
+                </div>
 
+            </nav>
+        </div>
     </main>
+
 </template>
