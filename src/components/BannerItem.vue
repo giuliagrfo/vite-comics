233f151a58b57fb6b93dc59e0b0db38e2eab7db3
giuliagrfo <giuliagrifo1@gmail.com>
2022-11-11 16:55:06 +0100

    add component - banner - bonus

---
+++ src/components/BannerItem.vue
@@ -0,0 +1,51 @@
+<script>
+export default {
+    name: 'Banner',
+    data() {
+        return {
+            itemsList: [
+                {
+                    image: 'buy-comics-digital-comics.png',
+                    text: 'digital comics',
+                },
+                {
+                    image: 'buy-comics-merchandise.png',
+                    text: 'dc merchandise',
+                },
+                {
+                    image: 'buy-comics-shop-locator.png',
+                    text: 'subescription',
+                },
+                {
+                    image: 'buy-comics-subscriptions.png',
+                    text: 'comic shop locator',
+                },
+                {
+                    image: 'buy-dc-power-visa.svg',
+                    text: 'dc power visa',
+                }
+            ]
+        }
+    },
+    methods: {
+        getImageUrl(name) {
+            return new URL(`../assets/img/${name}`, import.meta.url).href
+        }
+    }
+}
+
+</script>
+
+
+<template>
+    <nav class="banner">
+        <div class="container">
+            <a href="#" v-for="item in itemsList">
+                <img :src="getImageUrl(item.image)" alt="">
+                <span>{{ item.text }}</span>
+            </a>
+        </div>
+
+    </nav>
+</template>
+
--- src/components/SiteMain.vue
+++ src/components/SiteMain.vue
@@ -1,9 +1,12 @@
 <script>
-import CardsList from './CardsList.vue'
+import CardsList from './CardsList.vue';
+import Banner from './BannerItem.vue';
+
 export default {
     name: 'SiteMain',
     components: {
-        CardsList
+        CardsList,
+        Banner
     }
 }
 
@@ -18,32 +21,8 @@ export default {
             </div>
             <CardsList />
 
+            <Banner />
 
-            <nav class="navbar">
-                <div class="container">
-                    <a href="#">
-                        <img src="../assets/img/buy-comics-digital-comics.png" alt="">
-                        Digital Comic
-                    </a>
-                    <a href="#">
-                        <img src="../assets/img/buy-comics-merchandise.png" alt="">
-                        DC Merchandise
-                    </a>
-                    <a href="#">
-                        <img src="../assets/img/buy-comics-subscriptions.png" alt="">
-                        Subscription
-                    </a>
-                    <a href="#">
-                        <img src="../assets/img/buy-comics-shop-locator.png" alt="">
-                        Comic Shop Locator
-                    </a>
-                    <a href="#">
-                        <img src="../assets/img/buy-dc-power-visa.svg" alt="">
-                        DC Power Visa
-                    </a>
-                </div>
-
-            </nav>
         </div>
     </main>
 
